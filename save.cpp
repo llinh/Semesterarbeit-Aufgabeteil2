e32c6c1374573f84443c2f230fa84a5395629132
llinh <36013098+llinh@users.noreply.github.com>
2018-09-27 12:18:46 +0000

    Delete save.cpp

--- save.cpp
+++
@@ -1,9 +0,0 @@
-#include "save.h"
-
-void save::SaveNow(QPointF playerPos, int points, QList<Item> itemList)
-{
-    this->playerPos = playerPos;
-    this->points = points;
-    this->itemList = itemList;
-}
-save::save(){}
