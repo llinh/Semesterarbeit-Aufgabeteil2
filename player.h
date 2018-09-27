5dd97a653b4545f313d9b2e355abd0f81fe28102
llinh <36013098+llinh@users.noreply.github.com>
2018-09-27 14:22:04 +0200

    Add files via upload

---
+++ player.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <QtWidgets/QMainWindow>
+
+class player
+{
+public:
+    player();
+private:
+    int points;
+    QPointF pos;
+};
+
+#endif // PLAYER_H
